196cacd9873fe6b5e6a9a552d459589637e8dc22
mad420-max <78628376+mad420-max@users.noreply.github.com>
2021-10-27 07:39:34 +0600

    hello.js

---
+++ Basic_js/hello.js
@@ -0,0 +1,14 @@
+
+
+// output functions -> functions that will help you to show output
+// document.write(), console.log(), alert()
+// console.clear(), console.error(), console.info(), console.warn()
+
+document.write("hello world");
+document.write("<h1>Welcome to JS Program</h1>");
+
+
+console.log("hello world");
+
+//this alert() will display an alert message
+alert("welcome to js program");
