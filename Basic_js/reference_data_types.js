c13e39626c4f84f3156b66a622d647eb048b0140
mad420-max <78628376+mad420-max@users.noreply.github.com>
2021-10-27 09:29:50 +0600

    Create reference_data_types.js

---
+++ Basic_js/reference_data_types.js
@@ -0,0 +1,20 @@
+ //reference data type:arrays and object.
+
+
+//arrays
+
+var array = [1,2,3,4,5,6,7,8,]
+console.log(array);
+ //and
+ //arrays start from 0
+ console.log(array[0]);
+
+//object
+
+var marks ={
+  emon: 50,
+  tawhid:40,
+  rahim:30
+}
+console.log(marks);
+
