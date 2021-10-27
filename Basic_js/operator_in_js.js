c014daa9c00c2add0a39aaddb38095d980e2902e
mad420-max <78628376+mad420-max@users.noreply.github.com>
2021-10-27 11:03:46 +0600

    Create operator_in_js.js

---
+++ Basic_js/operator_in_js.js
@@ -0,0 +1,53 @@
+ //Arithmetic operators
+ var a = 12;
+ var b = 12
+ 
+ console.log("the value of a+b is:",a+b);
+ 
+ console.log("the value of a-b is:",a-b);
+ 
+ console.log("the value of a*b is:",a*b);
+ 
+//Assignment Operator
+
+var a = 2;
+var b = 3;
+
+var c = b;
+console.log(c);
+d = c+2 // c = 3
+console.log(d);
+ d -= 3; //d = 5
+// d *= 4;
+// d -= 2;
+ console.log(d);
+ 
+ 
+ //comparison operator
+ 
+ var x = 43;
+ var y = 12;
+ 
+ console.log(x==y);
+ //var x is not equal var y;so it's false.
+ var a = 8;
+ var b = 9;
+ console.log(a<b);//output is true beacuse 8<9.
+ 
+ //logical operator
+ 
+ // && = logical and, ||= logial or,
+ 
+ //logical and &&
+ console.log(true && true);
+ console.log(false&&true);
+ 
+ // logical or||
+ console.log(false||false);
+ console.log(true|| flase);
+ 
+ //logical not!
+ 
+ console.log(!true);
+ //output is false.true will be false and flase will be true in logical not
+ 
