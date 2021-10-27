4810fe78caf3633a8af6788d847e2d7a10a6b2c3
mad420-max <78628376+mad420-max@users.noreply.github.com>
2021-10-27 09:30:57 +0600

    Update primitive data types.js

--- Basic_js/primitive data types.js
+++ Basic_js/primitive data types.js
@@ -1,5 +1,4 @@
-1.primitive data type:undefind,null,
-number,string,boolean,symbool.
+1.primitive data type:undefind,null,number,string,boolean,symbool.
 
 
 //Number
@@ -12,14 +11,7 @@ var num2 = 22;
 var str = "this is string";
 var str = "this is also a string";
 
-//object
 
-var marks ={
-  emon: 50,
-  tawhid:40,
-  rahim:30
-}
-console.log(marks);
 
 //boolean
 
