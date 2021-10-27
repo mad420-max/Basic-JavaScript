288e6b096e8349a1777a97ad468a750f2003f545
mad420-max <78628376+mad420-max@users.noreply.github.com>
2021-10-27 09:27:45 +0600

    Create primitive data types.js

---
+++ Basic_js/primitive data types.js
@@ -0,0 +1,52 @@
+1.primitive data type:undefind,null,
+number,string,boolean,symbool.
+
+
+//Number
+
+var num1 = 21;
+var num2 = 22;
+
+//string
+
+var str = "this is string";
+var str = "this is also a string";
+
+//object
+
+var marks ={
+  emon: 50,
+  tawhid:40,
+  rahim:30
+}
+console.log(marks);
+
+//boolean
+
+var a = true;
+var b = false;
+
+console.log(a);
+console.log(b);
+//also 
+console.log(a,b);
+
+//undefind 
+
+var und = undefined;
+console.log(und);
+
+//and
+//und = undefind.
+
+var und;
+console.log(und);
+
+//null
+
+var a = null;
+
+console.log(a);
+
+
+//arrays
