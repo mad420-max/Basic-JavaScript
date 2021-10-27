3af3d6fb5f7639e9a895749dcb11d71b31dded12
mad420-max <78628376+mad420-max@users.noreply.github.com>
2021-10-27 11:23:54 +0600

    Update function.js

--- Basic_js/function.js
+++ Basic_js/function.js
@@ -1,1 +1,13 @@
 
+//javascript function
+
+function avg(a,b) {
+       //avg function name
+      c =(a+b)/2;
+      return c;
+
+    // return c = (a+b)/2;
+
+}
+c1 = avg(12,22); //a = 12, b = 22.just for example
+console.log(c1);
